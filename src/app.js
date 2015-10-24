d5c62bfeeae0a4c2e2c37127e49774aaf6c87b99
CosX <karl.f91@gmail.com>
2015-10-24 13:33:32 +0200

    name validation

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ let modal = document.querySelectorAll(".modal-container")[0];"modal-container"
 let form = document.getElementById("AnimalForm");
 form.addEventListener("submit", function (event) {
 	event.preventDefault();
-	modal.style.display = 'none';
-	
-	new Play.Playground(document.getElementById("name").value); 
+	if(document.getElementById("name").value !== ""){
+		modal.style.display = 'none';
+		new Play.Playground(document.getElementById("name").value); 
+	} else{
+		alert("Enter name plz!");
+	}
 });
